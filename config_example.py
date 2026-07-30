68106130505540abf4626a9ca90c7bae3a0e0efb
Alex <alexandreshofstall@yahoo.fr>
2026-07-30 20:14:31 +0200

    Adjustments

---
+++ config_example.py
@@ -0,0 +1,11 @@
+config_dict = {
+    "SEARCH_URL": "https://civiweb-api-prd.azurewebsites.net/api/Offers/search",
+    "HEADERS" : {
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Origin": "https://mon-vie-via.businessfrance.fr",
+            "Referer": "https://mon-vie-via.businessfrance.fr/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+            "X-API-KEY": "YOU API KEY HERE, CHECK FROM YOUR BROWSER"
+        }
+}
